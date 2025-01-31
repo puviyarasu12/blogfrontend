f06eb379d91ae5e141356a4a7f36e0f48bfd1240
puviyarasu12 <153293360+puviyarasu12@users.noreply.github.com>
2025-01-31 15:04:34 +0530

    Update Endpoint.js

--- src/services/Endpoint.js
+++ src/services/Endpoint.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BaseUrl='http://localhost:5000'
+export const BaseUrl='https://blog-backend-aead.onrender.com'
 const instance = axios.create({
     baseURL:BaseUrl,
     // headers: {
